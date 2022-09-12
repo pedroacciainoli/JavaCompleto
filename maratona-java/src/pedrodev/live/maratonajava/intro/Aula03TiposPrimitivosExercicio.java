68cd683234ea8b5f08f021e318bbe6ac7096516e
pedro.acciainoli <pedro.acciainoli@gmail.com>
2022-09-12 17:14:16 -0300

    exercicio tipos primitivos

---
+++ maratona-java/src/pedrodev/live/maratonajava/intro/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,18 @@
+package pedrodev.live.maratonajava.intro;
+
+/*
+Crie variáveis para os compos descritos abaixo entre <> e imprima a seguinte mensagem:
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o salário de <salario>, na data <data>
+ */
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Pedro";
+        String addressName = "Rua Aporema";
+        float salary = 2500;
+        String date = "25/03/1994";
+
+        System.out.println("Eu "+name+", morando no endereço "+addressName+", confirmo que recebi o salário de "+salary+", na data "+date);
+    }
+}
