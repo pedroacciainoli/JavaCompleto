199aa3739e97aa7fed3c2ace0112d8e5166482f6
pedro.acciainoli <pedro.acciainoli@gmail.com>
2022-09-12 16:31:20 -0300

    comentarios ide

--- maratona-java/src/pedrodev/live/maratonajava/intro/OlaDevDojo.java
+++ maratona-java/src/pedrodev/live/maratonajava/intro/OlaDevDojo.java
@@ -1,6 +1,16 @@
 package pedrodev.live.maratonajava.intro;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    // comentário
+    /* linhas
+    multiplas linhas
+
+     */
+
+
     public static void main(String[] args){
         System.out.println("Olá Mundo");
     }
